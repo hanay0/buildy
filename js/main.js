d534a44c79a9694e8b72f9a7a93eb58704deaef8
hanafy0 <30327222+hanay0@users.noreply.github.com>
2021-02-07 00:32:15 +0200

    finished desktop viewport

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// slick first slider
+$(document).ready(function(){
+    $('.slides').slick({
+        prevArrow: '.prev',
+        nextArrow: '.next'
+            });
+        });
+
+
+        $(document).ready(function(){
+            $('.testimonial-wrapper').slick({
+                prevArrow: '.arrow-left',
+                nextArrow: '.arrow-right'
+                    });
+                });
+        
